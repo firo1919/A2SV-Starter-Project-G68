cb702272d2f7024e00f6e0f51ae641456d30357e
Israel Sol <isrucrasus10@gmail.com>
2025-08-05 12:05:12 +0300

    feat: add register user route

---
+++ src/types/auth.ts
@@ -0,0 +1,15 @@
+export interface User {
+  full_name: string;
+  email: string;
+  password: string;
+}
+
+export interface AuthResponse {
+  success: boolean;
+  data?: {
+    id: string;
+    full_name: string;
+    email: string;
+  };
+  message: string;
+}
