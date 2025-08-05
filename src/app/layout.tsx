8d7a610b0045c7fd306977f8b66b44451c0e2af2
firomsa <firomsassf@gmail.com>
2025-08-05 22:57:32 +0300

    feat: added style configs in globalcss

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,25 @@
 import AuthProvider from "@/providers/AuthProvider";
 import "@/styles/globals.css";
 import type { Metadata } from "next";
+import { Inter, Poppins } from "next/font/google";
 
 export const metadata: Metadata = {
 	title: "Application Platform",
 	description: "An application platform for A2SV",
 };
+
+const poppins = Poppins({
+	display: "swap",
+	subsets: ["latin"],
+	variable: "--font-poppins",
+	weight: ["100", "200", "300", "400", "500", "600"],
+});
+
+const inter = Inter({
+	display: "swap",
+	subsets: ["latin"],
+	variable: "--font-inter",
+});
 
 export default function RootLayout({
 	children,
@@ -15,7 +29,7 @@ export default function RootLayout({
 	return (
 		<html lang="en">
 			<AuthProvider>
-				<body>{children}</body>
+				<body className={`${poppins.variable} ${inter.variable}`}>{children}</body>
 			</AuthProvider>
 		</html>
 	);
