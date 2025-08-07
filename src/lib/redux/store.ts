db611b9aa4619d8e85ceca28462bf825beff7602
firomsa <firomsassf@gmail.com>
2025-08-07 11:10:17 +0300

    feat: added create application mutation in applications api slice

--- src/lib/redux/store.ts
+++ src/lib/redux/store.ts
@@ -1,11 +1,14 @@
 import { configureStore } from "@reduxjs/toolkit";
+import { applicationsAPi } from "./api/applicationsApiSlice";
 import { usersApi } from "./api/usersApiSlice";
 
 export const store = configureStore({
 	reducer: {
 		[usersApi.reducerPath]: usersApi.reducer,
+		[applicationsAPi.reducerPath]: applicationsAPi.reducer,
 	},
-	middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(usersApi.middleware),
+	middleware: (getDefaultMiddleware) =>
+		getDefaultMiddleware().concat(usersApi.middleware, applicationsAPi.middleware),
 });
 
 export type RootState = ReturnType<typeof store.getState>;
