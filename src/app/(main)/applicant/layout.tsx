4d8cb123d922c82ffbb6cd1e955f2924044d9e23
annah11 <hanamesfin67@gmail.com>
2025-08-07 06:45:21 +0300

    Update applicant card component and layout

--- src/app/(main)/applicant/layout.tsx
+++ src/app/(main)/applicant/layout.tsx
@@ -1,9 +1,9 @@
 import React from "react";
 
 export default function ApplicantLayout({
-    children,
+	children,
 }: Readonly<{
-    children: React.ReactNode;
+	children: React.ReactNode;
 }>) {
-    return <div>{children}</div>;
+	return <div>{children}</div>;
 }
