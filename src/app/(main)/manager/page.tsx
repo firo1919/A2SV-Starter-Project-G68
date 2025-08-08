17a371fc193c976ba9854f6b976780f8a80f595b
Fozia31 <foziyadamtew00@gmail.com>
2025-08-08 14:05:37 -0700

    chore: create Home Page component

---
+++ src/app/(main)/manager/page.tsx
@@ -0,0 +1,14 @@
+
+
+import React from 'react'
+import HomePage from '@/app/components/HomePage'
+
+const page = () => {
+  return (
+    <div>
+      <HomePage/>
+    </div>
+  )
+}
+
+export default page
