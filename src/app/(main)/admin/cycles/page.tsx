2e163577a61ebbf4a37b0a04aa2973f7e2a27ca7
ABENEZER-SELESHI <ebenezerseleshi@gmail.com>
2025-08-11 18:37:46 -0700

    feat: finished admin page

--- src/app/(main)/admin/cycles/page.tsx
+++ src/app/(main)/admin/cycles/page.tsx
@@ -1,10 +1,10 @@
-import { getData } from "@/utils/adminUtils";
 import AdminCyclesClient from "@/app/components/admin/Cycles/ClientAdminCycles";
+import { getData } from "@/utils/adminUtils";
 
 const Page = async () => {
 	const res = await getData();
 	const cycles = res?.data?.cycles ?? [];
-	const total = res?.data.total_count;
+	const total = res?.data.total_count || null;
 
 	return (
 		<div>
